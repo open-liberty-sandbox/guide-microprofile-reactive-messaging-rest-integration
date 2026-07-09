1a0fd750e88581a4e2389d499a92963a584158e0
Manu Paul Kunnumpurathu <kun.manu93@gmail.com>
2026-07-09 13:50:56 +0200

    feat: add an event processor to a reactive service

--- start/system/src/main/java/io/openliberty/guides/system/SystemService.java
+++ start/system/src/main/java/io/openliberty/guides/system/SystemService.java
@@ -1,14 +1,3 @@
-// tag::copyright[]
-/*******************************************************************************
- * Copyright (c) 2020, 2024 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *******************************************************************************/
-// end::copyright[]
 package io.openliberty.guides.system;
 
 import java.lang.management.ManagementFactory;
@@ -17,14 +6,20 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+
 import jakarta.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.reactivestreams.Publisher;
+
+import io.openliberty.guides.models.PropertyMessage;
 import io.openliberty.guides.models.SystemLoad;
 import io.reactivex.rxjava3.core.Flowable;
 
 @ApplicationScoped
 public class SystemService {
+
     private static Logger logger = Logger.getLogger(SystemService.class.getName());
 
     private static final OperatingSystemMXBean OS_MEAN =
@@ -45,7 +40,21 @@ public class SystemService {
     @Outgoing("systemLoad")
     public Publisher<SystemLoad> sendSystemLoad() {
         return Flowable.interval(15, TimeUnit.SECONDS)
-                       .map((interval -> new SystemLoad(getHostname(),
-                             OS_MEAN.getSystemLoadAverage())));
+                .map((interval -> new SystemLoad(getHostname(),
+                        OS_MEAN.getSystemLoadAverage())));
+    }
+
+    @Incoming("propertyRequest")
+    @Outgoing("propertyResponse")
+    public PropertyMessage sendProperty(String propertyName) {
+        logger.info("sendProperty: " + propertyName);
+        if (propertyName == null || propertyName.isEmpty()) {
+            logger.warning(propertyName == null ? "Null" : "An empty string"
+                    + " is not System property.");
+            return null;
+        }
+        return new PropertyMessage(getHostname(),
+                propertyName,
+                System.getProperty(propertyName, "unknown"));
     }
 }
