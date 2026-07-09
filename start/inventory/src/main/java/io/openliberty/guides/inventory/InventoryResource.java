e5fdb245c4dfaa32c4a044cac637ca3a4d6d2f71
Manu Paul Kunnumpurathu <kun.manu93@gmail.com>
2026-07-09 13:39:51 +0200

    refactor: refactor InventoryResource
    - add a REST endpoints that produce and consume events

--- start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -1,14 +1,3 @@
-// tag::copyright[]
-/*******************************************************************************
- * Copyright (c) 2020, 2024 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *******************************************************************************/
-// end::copyright[]
 package io.openliberty.guides.inventory;
 
 import java.util.List;
@@ -19,8 +8,10 @@ import java.util.stream.Collectors;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -28,7 +19,14 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.reactivestreams.Publisher;
+
+import io.openliberty.guides.models.PropertyMessage;
 import io.openliberty.guides.models.SystemLoad;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.FlowableEmitter;
 
 
 @ApplicationScoped
@@ -36,6 +34,7 @@ import io.openliberty.guides.models.SystemLoad;
 public class InventoryResource {
 
     private static Logger logger = Logger.getLogger(InventoryResource.class.getName());
+    private FlowableEmitter<String> propertyNameEmitter;
 
     @Inject
     private InventoryManager manager;
@@ -45,12 +44,12 @@ public class InventoryResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getSystems() {
         List<Properties> systems = manager.getSystems()
-                                          .values()
-                                          .stream()
-                                          .collect(Collectors.toList());
+                .values()
+                .stream()
+                .collect(Collectors.toList());
         return Response.status(Response.Status.OK)
-                       .entity(systems)
-                       .build();
+                .entity(systems)
+                .build();
     }
 
     @GET
@@ -60,12 +59,25 @@ public class InventoryResource {
         Optional<Properties> system = manager.getSystem(hostname);
         if (system.isPresent()) {
             return Response.status(Response.Status.OK)
-                           .entity(system)
-                           .build();
+                    .entity(system)
+                    .build();
         }
         return Response.status(Response.Status.NOT_FOUND)
-                       .entity("hostname does not exist.")
-                       .build();
+                .entity("hostname does not exist.")
+                .build();
+    }
+
+    @PUT
+    @Path("/data")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response updateSystemProperty(String propertyName) {
+        logger.info("updateSystemProperty: " + propertyName);
+        propertyNameEmitter.onNext(propertyName);
+        return Response
+                .status(Response.Status.OK)
+                .entity("Request successful for the " + propertyName + " property\n")
+                .build();
     }
 
     @DELETE
@@ -73,7 +85,7 @@ public class InventoryResource {
     public Response resetSystems() {
         manager.resetSystems();
         return Response.status(Response.Status.OK)
-                       .build();
+                .build();
     }
 
     @Incoming("systemLoad")
@@ -86,5 +98,25 @@ public class InventoryResource {
             manager.addSystem(hostname, sl.loadAverage);
             logger.info("Host " + hostname + " was added: " + sl);
         }
+    }
+
+    @Incoming("addSystemProperty")
+    public void getPropertyMessage(PropertyMessage pm)  {
+        logger.info("getPropertyMessage: " + pm);
+        String hostId = pm.hostname;
+        if (manager.getSystem(hostId).isPresent()) {
+            manager.updatePropertyMessage(hostId, pm.key, pm.value);
+            logger.info("Host " + hostId + " was updated: " + pm);
+        } else {
+            manager.addSystem(hostId, pm.key, pm.value);
+            logger.info("Host " + hostId + " was added: " + pm);
+        }
+    }
+
+    @Outgoing("requestSystemProperty")
+    public Publisher<String> sendPropertyName() {
+        Flowable<String> flowable = Flowable.<String>create(emitter ->
+                this.propertyNameEmitter = emitter, BackpressureStrategy.BUFFER);
+        return flowable;
     }
 }
